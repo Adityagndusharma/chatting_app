0ed9a9a493e9db4fce6b4d6b79f939d76edb0979
Aditya Sharma <96620736+Adityagndusharma@users.noreply.github.com>
2023-08-23 19:36:37 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>FunSkoot - Talk any time anywhere</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<style>
+body {
+  background-image: url('https://cdn5.f-cdn.com/contestentries/2046262/32789691/61ee7284184a9_thumb900.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<body>
+</body>
